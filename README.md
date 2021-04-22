70ed8e376333572e0a9cb63f24608ba0845aa56c
majinlong <qq111222>
2021-04-22 16:23:17 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,1 @@
+# feith_generator
---
+++ lib/FeignGenerator.dart
@@ -0,0 +1,4 @@
+import 'package:build/build.dart';
+import 'src/FeignClientGenerator.dart';
+
+Builder feignBuilder(BuilderOptions options) => generatorFactoryBuilder(options);
